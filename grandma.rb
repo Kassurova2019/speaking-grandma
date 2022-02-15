43b3181fc2f8d14aad5cd179d1917c07edeab1c9
Luba Kassurova <kassurova@icloud.com>
2022-02-15 16:34:24 -0500

    commit tree a991a06ab75e9af0d47c31f51088bd3a1577dbc4

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,17 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma(string)
+if string == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+elsif string == "Hi Nana, how are you?"
+     "HUH?! SPEAK UP, SONNY!"
+elsif string == "Hi!"
+    "HUH?! SPEAK UP, SONNY!"
+elsif string == "WHAT DID YOU EAT TODAY?"
+    "NO, NOT SINCE 1938!"
+else 
+    "NO, NOT SINCE 1938!"
+
+end
+end
